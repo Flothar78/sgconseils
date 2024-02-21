fb3430eb187d38940949bfaa0a894881c6060851
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2024-02-21 17:41:44 +0100

    Add reactive modal menu
    Get button "MENU" clickable.
    "MENU" trigger  adds reactive class 'active' to menu-list.
    Class 'active' move element menu-list to the left making it visible.

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,25 +4,53 @@
       src="~/assets/img/logo_sg_conseils.png"
       alt="logo sg conseils format png"
     />
-    <div class="menu-title">MENU</div>
+    <button @click="menuListSlideIn()" class="menu-button">MENU</button>
+    <ul class="menu-list" :class="{ active: isActive }">
+      <li>Accueil</li>
+      <li>Nos Métiers</li>
+      <li>Contact</li>
+    </ul>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const isActive = ref(false);
+const menuListSlideIn = () => {
+  isActive.value = !isActive.value;
+};
+</script>
 
-<style scoped>
+<style lang="scss" scoped>
 .navbar-container {
   position: fixed;
   display: flex;
   justify-content: space-between;
+  align-items: center;
   background: linear-gradient(0.25turn, #b12434, #858384);
   height: 10vh;
   width: 100%;
-  z-index:30;
+  z-index: 30;
+  button {
+    background: transparent;
+  }
 }
-.menu-title {
+.menu-button {
   color: white;
-  margin: 2rem 1.83rem 0 0;
   font-size: 1.3rem;
+}
+.menu-list {
+  height: 10vh;
+  list-style: none;
+  position: fixed;
+  right: -12rem;
+  margin-top: 25vh;
+  padding: 1.5rem 0.3rem 0 0.6rem;
+  background-color: #b3f2dd;
+  display: flex;
+  flex-direction: column;
+}
+.active {
+  right: 0vw;
 }
 </style>
