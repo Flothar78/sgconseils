605723b146aa3455f44ac8565af43474d8b3941b
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2024-02-22 16:13:48 +0100

    Add Scroll Button Add scroll button Make it twinkle and stop twinkling on click Emit method from button component to parent. Make button to scroll down to next div (Expertises-cards)

---
+++ components/ScrollButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    class="scroll-button"
+    @click="scrollDown()"
+    :class="{ twinkle: isActive }"
+  >
+    <div class="arrow-down"></div>
+    <div class="arrow-down"></div>
+    <div class="arrow-down"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+const isActive = ref(true);
+const emit = defineEmits(["scrollTo"]);
+const scrollDown = () => {
+  //isActive.value = false;
+  emit("scrollTo");
+};
+</script>
+
+<style lang="scss" scoped>
+@keyframes flash {
+  0% {
+    opacity: 1;
+  }
+  75% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.twinkle {
+  animation: flash 1s infinite ease-in-out;
+}
+.scroll-button {
+  border: 8px #99d17b solid;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  position: absolute;
+  right: 46.9%;
+  top: 85%;
+  .arrow-down {
+    border: 5px #99d17b solid;
+    border-width: 0 0.2em 0.2em 0;
+    margin: 0.5em 0 -0.5em 1.2em;
+    height: 15%;
+    width: 15%;
+    transform: rotate(45deg);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
   <NavBar />
-  <div class="hero">Home page</div>
-  <div class="expertises-cards">
+  <div class="hero" @scrollTo="scrollToTopOfCards">
+    <img
+      src="assets/img/background-image-metier.jpeg"
+      alt="desk picture in background"
+    />
+    <ScrollButton @scrollTo="scrollToTopOfCards" />
+  </div>
+  <div class="expertises-cards" ref="cardsDiv">
     <div>
       <NuxtLink :to="`/metiers/juridique`">Page juridique</NuxtLink>
     </div>
@@ -26,20 +32,36 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const cardsDiv = ref(null);
+const scrollToTopOfCards = () => {
+  const div = cardsDiv.value;
+  console.log(div);
+  div.scrollIntoView({ behavior: "smooth" });
+};
+</script>
 
-<style scoped>
+<style lang="scss" scoped>
 .hero {
   padding-top: 12vh;
   position: relative;
   background-color: #425b8a;
   color: white;
-  min-height: 90vh;
+  height: 92vh;
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    margin-top: -3vh;
+    filter: grayscale(70%) contrast(25%);
+    opacity: 0.3;
+  }
 }
 .expertises-cards {
   border: black 1px solid;
   background-color: #425b8a;
-  min-height: 50vh;
+  height: 150vh;
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
