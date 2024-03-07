bf20e41a924df14d0f0523edf56912341a8b3cb3
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2024-03-07 12:37:59 +0100

    Modify menu
    Add links to metiers/id
    Modify CSS

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,22 +7,46 @@
     /></NuxtLink>
     <button @click.stop="menuListSlide()" class="menu-button">MENU</button>
     <div class="menu-list" :class="{ active: isActive }">
-      <NuxtLink class="link-item" :to="`/#accueil`">Accueil</NuxtLink>
-      <NuxtLink class="link-item" :to="`/#metiers`">Nos Métiers</NuxtLink>
-      <NuxtLink class="link-item" :to="`/contact`">Contact</NuxtLink>
+      <NuxtLink class="link-item" :to="`/#accueil`">ACCUEIL</NuxtLink>
+      <NuxtLink class="link-item" id="link-item-metiers" :to="`/#metiers`"
+        >EXPERTISES</NuxtLink
+      >
+      <NuxtLink class="sublink-item" :to="`/metiers/comptabilite`"
+        >Comptabilité</NuxtLink
+      >
+      <NuxtLink class="sublink-item" :to="`/metiers/audit`">Audit</NuxtLink
+      ><NuxtLink class="sublink-item" :to="`/metiers/juridique`"
+        >Juridique</NuxtLink
+      >
+      <NuxtLink class="sublink-item" :to="`/metiers/conseil`">Conseil</NuxtLink>
+      <NuxtLink class="sublink-item" :to="`/metiers/gestion`">Gestion</NuxtLink
+      ><NuxtLink class="sublink-item" :to="`/metiers/social`">Social</NuxtLink>
+      <NuxtLink class="sublink-item" :to="`/metiers/informatique`"
+        >Informatique</NuxtLink
+      >
+      <NuxtLink class="link-item" :to="`/contact`">CONTACT</NuxtLink>
     </div>
   </header>
   <slot />
   <footer class="footer">
     <div class="footer-div">
-      <NuxtLink class="footer-title" >NOS COMPETENCES:</NuxtLink>
-      <NuxtLink class="footer-infos" :to="`/metiers/comptabilite`" >Comptabilité</NuxtLink>
-      <NuxtLink class="footer-infos" :to="`/metiers/juridique`" >Juridique</NuxtLink>
-      <NuxtLink class="footer-infos" :to="`/metiers/audit`" >Audit</NuxtLink>
-      <NuxtLink class="footer-infos" :to="`/metiers/gestion`" >Gestion</NuxtLink>
-      <NuxtLink class="footer-infos" :to="`/metiers/social`" >Social / RH</NuxtLink>
-      <NuxtLink class="footer-infos" :to="`/metiers/conseil`" >Conseil</NuxtLink>
-      <NuxtLink class="footer-infos" :to="`/metiers/informatique`" >Informatique</NuxtLink>
+      <NuxtLink class="footer-title">NOS COMPETENCES:</NuxtLink>
+      <NuxtLink class="footer-infos" :to="`/metiers/comptabilite`"
+        >Comptabilité</NuxtLink
+      >
+      <NuxtLink class="footer-infos" :to="`/metiers/audit`">Audit</NuxtLink>
+      <NuxtLink class="footer-infos" :to="`/metiers/juridique`"
+        >Juridique</NuxtLink
+      >
+      <NuxtLink class="footer-infos" :to="`/metiers/conseil`">Conseil</NuxtLink>
+      <NuxtLink class="footer-infos" :to="`/metiers/gestion`">Gestion</NuxtLink>
+      <NuxtLink class="footer-infos" :to="`/metiers/social`"
+        >Social / RH</NuxtLink
+      >
+
+      <NuxtLink class="footer-infos" :to="`/metiers/informatique`"
+        >Informatique</NuxtLink
+      >
     </div>
     <div class="footer-div">
       <div class="footer-title">NOUS CONTACTER:</div>
@@ -47,13 +71,19 @@ const menuListSlide = () => {
 </script>
 
 <style lang="scss" scoped>
-$base-blue: #9fb1d1;
+$base-blue: #7d93b5;
+$dark-blue: #425b8a;
+$darkest-blue: #1c212f;
+$base-red: #b12434;
+$base-white: #bec8da;
+$base-black: #191c20;
+$base-grey: #858384;
 .navbar-container {
   position: fixed;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background: linear-gradient(145deg, #b12434, #858384);
+  background: linear-gradient(145deg, $base-red, $base-grey);
   height: 10vh;
   width: 100%;
   padding-left: 0.3rem;
@@ -64,42 +94,54 @@ $base-blue: #9fb1d1;
   }
   .menu-button {
     margin-right: 2rem;
-    background: #b12434;
+    background: $base-red;
     border: 1px transparent solid;
     color: white;
     font-size: 1rem;
     padding: 0.5rem;
-    box-shadow: 4px 4px 5px #57000a;
+    box-shadow: 4px 4px 5px $base-black;
   }
   .menu-button:hover {
-    box-shadow: 1px 1px 2px #57000a;
+    box-shadow: 1px 1px 2px $base-black;
   }
 }
 .menu-list {
-  background-color: white;
-  border: 2px solid black;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
-  font-size: 0.8rem;
-  width: 9vw;
-  height: 10vh;
+  justify-content: center;
+  background-color: white;
+  border: 2px solid $base-black;
+  font-size: 1rem;
+  width: 12vw;
+  height: 30vh;
   list-style: none;
   position: fixed;
   right: -12rem;
   margin-right: -1rem;
-  margin-top: 25vh;
-  padding: 0.7rem 1rem 0.7rem 0.6rem;
+  margin-top: 54vh;
+  padding: 0.7rem 1rem;
   display: flex;
   flex-direction: column;
   transition: right 0.5s ease;
   border-radius: 10% 0 0 10%;
+  #link-item-metiers {
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 0.4rem;
+  }
   .link-item {
     text-decoration: none;
-    color: #b12434;
+    color: $base-black;
     font-family: Arial, sans-serif;
     font-size: 1rem;
     font-weight: 800;
+    padding: 0.5rem 0;
+  }
+  .sublink-item {
+    text-decoration: none;
+    color: $base-black;
+    font-size: 0.8rem;
+    padding: 0.2rem 0 0 0.4rem;
   }
 }
 .active {
