37ae4319d6dab302dd7ecf3f57aad411f5aa1075
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2024-03-15 15:14:07 +0100

    Reset contact page DOM
    Simplify it with the same 3 depths in both form and map container
    Put anchors of each container at the start
    Take off height of full page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,64 +1,65 @@
 <template>
   <div class="global-contact-page">
-    <div class="contact-title">
-      Pour toutes vos questions juridiques, comptables, sociales, de gestion, de
-      conseil:
+    <div class="form-and-anchor-container" id="form-anchor">
+      <div class="contact-title">
+        Pour toutes vos questions juridiques, comptables, sociales, de gestion,
+        de conseil:
+        <div style="padding-left: 2rem; font-size: 1.2rem">
+          Contactez nos consultants:
+        </div>
+      </div>
+      <Form @submit="onSubmit" class="contact-form">
+        <div class="form-entry">
+          <div class="form-labels">Votre adresse e-mail:</div>
+          <Field
+            class="form-field"
+            type="email"
+            name="email"
+            :rules="validateEmail"
+          />
+          <div class="error-container">
+            <ErrorMessage name="email" class="error-message" />
+          </div>
+        </div>
+        <div class="form-entry">
+          <div class="form-labels">Votre société:</div>
+          <Field
+            class="form-field"
+            type="text"
+            name="company"
+            :rules="validateCompany"
+          />
+          <div class="error-container">
+            <ErrorMessage name="company" class="error-message" />
+          </div>
+        </div>
+        <div class="form-entry">
+          <div class="form-labels">Votre nom:</div>
+          <Field
+            class="form-field"
+            type="text"
+            name="lastname"
+            :rules="validateName"
+          />
+          <div class="error-container">
+            <ErrorMessage name="lastname" class="error-message" />
+          </div>
+        </div>
+        <div class="form-entry">
+          <div class="form-labels">Votre message:</div>
+          <textarea id="message"></textarea>
+        </div>
+        <button class="form-button" type="submit">Envoyer</button>
+      </Form>
+      <NuxtLink to="/contact/#map-anchor" id="scroll-to-map">
+        OpenStreetMap:</NuxtLink
+      >
     </div>
-    <div style="padding-left: 2rem; font-size: 1.2rem">
-      Contactez nos consultants:
-    </div>
-
-    <Form @submit="onSubmit" class="contact-form">
-      <div class="form-entry">
-        <div class="form-labels">Votre adresse e-mail:</div>
-        <Field
-          class="form-field"
-          type="email"
-          name="email"
-          :rules="validateEmail"
-        />
-        <div class="error-container">
-          <ErrorMessage name="email" class="error-message" />
-        </div>
-      </div>
-      <div class="form-entry">
-        <div class="form-labels">Votre société:</div>
-        <Field
-          class="form-field"
-          type="text"
-          name="company"
-          :rules="validateCompany"
-        />
-        <div class="error-container">
-          <ErrorMessage name="company" class="error-message" />
-        </div>
-      </div>
-      <div class="form-entry">
-        <div class="form-labels">Votre nom:</div>
-        <Field
-          class="form-field"
-          type="text"
-          name="lastname"
-          :rules="validateName"
-        />
-        <div class="error-container">
-          <ErrorMessage name="lastname" class="error-message" />
-        </div>
-      </div>
-      <div class="form-entry">
-        <div class="form-labels">Votre message:</div>
-        <textarea id="message"></textarea>
-      </div>
-      <button class="form-button" type="submit">Envoyer</button>
-    </Form>
-    <NuxtLink to="/contact/#scroll-to-form" id="scroll-to-map">
-      OpenStreetMap:</NuxtLink
-    >
-    <div href="#" id="map-anchor">
-      <NuxtLink href="#" to="/contact/#scroll-to-map" id="scroll-to-form"
+    <div class="map-and-anchor-container" id="map-anchor">
+      <NuxtLink href="#" to="/contact/#form-anchor" id="scroll-to-form"
         >Aller au formulaire
       </NuxtLink>
-      <div id="map-container">
+      <div class="map-container">
         <LMap
           ref="map"
           :zoom="zoom"
@@ -124,87 +125,96 @@ $base-red: #b12434;
 $base-white: #bec8da;
 $base-black: #191c20;
 .global-contact-page {
-  padding-top: 10vh;
   position: relative;
   background-color: #bec8da;
   color: $base-black;
-  height: 195vh;
+
   .contact-title {
     font-size: 1.2rem;
     font-weight: 700;
     margin: 2rem 0 1rem 1rem;
   }
-  .contact-form {
-    border: 12px solid $darkest-blue;
-    border-radius: 5%;
-    background: white;
-    height: 68vh;
-    width: 45%;
-    margin: 2rem 3rem 3rem 2.5rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    .form-entry {
+  .form-and-anchor-container {
+    padding-top: 7rem;
+    .contact-form {
+      border: 12px solid $darkest-blue;
+      border-radius: 5%;
+      background: white;
+      height: 68vh;
+      width: 45%;
+      margin: 2rem 3rem 3rem 2.5rem;
       display: flex;
       flex-direction: column;
-      padding: 5% 0 0 5%;
-      .form-labels {
-        color: $base-black;
-        padding-bottom: 0.5rem;
-      }
-      .form-field {
-        border: 2px solid $base-black;
-        width: 65%;
-        height: 3vh;
-      }
-      #message {
-        border: 2px solid $base-black;
-        width: 75%;
-        height: 12vh;
-      }
-      .error-container {
-        margin-top: 1rem;
-        height: 1rem;
-        .error-message {
-          color: red;
-          font-size: 1rem;
-          letter-spacing: 1.1px;
+      justify-content: flex-start;
+
+      .form-entry {
+        display: flex;
+        flex-direction: column;
+        padding: 5% 0 0 5%;
+        .form-labels {
+          color: $base-black;
+          padding-bottom: 0.5rem;
+        }
+        .form-field {
+          border: 2px solid $base-black;
+          width: 65%;
+          height: 3vh;
+        }
+        #message {
+          border: 2px solid $base-black;
+          width: 75%;
+          height: 12vh;
+        }
+        .error-container {
+          margin-top: 1rem;
+          height: 1rem;
+          .error-message {
+            color: red;
+            font-size: 1rem;
+            letter-spacing: 1.1px;
+          }
         }
       }
+      .form-button {
+        background: $base-red;
+        color: white;
+        font-size: 1.1rem;
+        font-weight: 900;
+        letter-spacing: 1.2px;
+        height: 8%;
+        width: 33%;
+        border-radius: 7%;
+        margin: auto;
+        box-shadow: 4px 4px 6px $base-black;
+      }
+      .form-button:hover {
+        box-shadow: 0 0 0;
+      }
     }
-    .form-button {
-      background: $base-red;
-      color: white;
-      font-size: 1.1rem;
-      font-weight: 900;
-      letter-spacing: 1.2px;
-      height: 8%;
-      width: 33%;
-      border-radius: 7%;
-      margin: auto;
-      box-shadow: 4px 4px 6px $base-black;
-    }
-    .form-button:hover {
-      box-shadow: 0 0 0;
+    #scroll-to-map {
+      text-decoration: none;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: $base-black;
+      background: white;
+      position: absolute;
+      right: 3vw;
+      top: 36vh;
+      border: 4px solid black;
+      height: 15vh;
+      width: 15vw;
     }
   }
-  #scroll-to-map {
-    text-decoration: none;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: $base-black;
-    background: white;
-    position: absolute;
-    right: 3vw;
-    top: 25vh;
-    border: 4px solid black;
-    height: 15vh;
-    width: 15vw;
-  }
-  #map-anchor {
-    border: solid 2px transparent;
-
+  .map-and-anchor-container {
+    padding-top: 20vh;
+    height: 90vh;
+    .map-container {
+      border: 12px solid $darkest-blue;
+      margin: 1rem 0 0 15rem;
+      height: 68vh;
+      width: 60%;
+    }
     #scroll-to-form {
       height: 15vh;
       width: 15vw;
@@ -215,15 +225,9 @@ $base-black: #191c20;
       color: $base-black;
       background: white;
       position: absolute;
-      bottom: 73vh;
+      bottom: 41vh;
       left: 4vw;
-      border: 4px solid black;
-    }
-    #map-container {
-      border: 12px solid $darkest-blue;
-      margin: 15vh 0 0 33vw;
-      height: 68vh;
-      width: 60%;
+      border: 4px solid $base-red;
     }
   }
   @media screen and (max-width: 767px) {
