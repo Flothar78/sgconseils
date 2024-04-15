54349df93797387eda7400f9be3be8b8581ab2a9
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2024-04-15 20:10:22 +0200

    Delete contact page for build and host

--- server/api/form.ts
+++ server/api/form.ts
@@ -1,12 +1,12 @@
-export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
-  body.mailObject = "Message envoyé depuis le site web sgconseils.com";
-  const emailContent = `
-   Société: ${body.company},
-   Message du client: ${body.message}
-   Adresse mail de contact: ${body.email},
-   Contact au sein de la société:  ${body.lastname}
-   Objet du message: ${body.mailObject}
-   `;
-  console.log(emailContent);
-});
+// export default defineEventHandler(async (event) => {
+//   const body = await readBody(event);
+//   body.mailObject = "Message envoyé depuis le site web sgconseils.com";
+//   const emailContent = `
+//    Société: ${body.company},
+//    Message du client: ${body.message}
+//    Adresse mail de contact: ${body.email},
+//    Contact au sein de la société:  ${body.lastname}
+//    Objet du message: ${body.mailObject}
+//    `;
+//   console.log(emailContent);
+// });
