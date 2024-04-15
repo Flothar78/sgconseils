e43271d8de2e7f01fc275d35d821cca46eb0a4b6
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2024-04-15 10:30:01 +0200

    Adjust CSS on scroll button small screen

--- pages/metiers/[id].vue
+++ pages/metiers/[id].vue
@@ -244,7 +244,7 @@ $base-black: #191c20;
         border-radius: 0;
         position: absolute;
         z-index: 20;
-        top: 0vh;
+        top: 10vh;
         left: 0%;
         transform: translateX(0%);
         justify-content: flex-end;
@@ -252,7 +252,7 @@ $base-black: #191c20;
         border: $darkest-blue 0 solid;
         background: $base-white;
         width: 100%;
-        height: 100%;
+        height: 90vh;
         padding: 2rem 0.7rem 2rem 1rem;
         .title {
           margin-bottom: 0.3rem;
