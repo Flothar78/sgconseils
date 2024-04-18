c25d209e80eb6ef6e2f8cd725917774c8d635918
Flothar78 <flothar78@flothar78-HP-Pavilion-Gaming-Laptop-15-ec1xxx.(none)>
2024-04-18 15:52:29 +0200

    Add CSS leaflet in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   alias: {
     "@": "/<rootDir>",
   },
-  css: ["~/assets/scss/main.css", "~/node_modules/leaflet/dist/leaflet.css"],
+  css: ["~/assets/scss/main.css", "../node_modules/leaflet/dist/leaflet.css"],
   modules: [
     "nuxt3-leaflet",
     "@nuxt/image",
